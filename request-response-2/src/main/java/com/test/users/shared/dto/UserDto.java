0e69c4d04e28f9d8bf7b2f11c1f5b4bdbaa4f863
dhanyn10 <dhanyn.dhan@gmail.com>
2019-08-29 16:09:01 +0700

    create dto

---
+++ request-response-2/src/main/java/com/test/users/shared/dto/UserDto.java
@@ -0,0 +1,73 @@
+package com.test.users.shared.dto;
+
+import java.io.Serializable;
+
+public class UserDto implements Serializable{
+	
+	private static final long serialVersionUID = -5453927472428408094L;
+	private long id;
+	private String userId;
+	private String firstName;
+	private String lastname;
+	private String email;
+	private String password;
+	private String encryptedpassword;
+	private String emailVerifycationToken;
+	private Boolean emailVerificationStatus;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEncryptedpassword() {
+		return encryptedpassword;
+	}
+	public void setEncryptedpassword(String encryptedpassword) {
+		this.encryptedpassword = encryptedpassword;
+	}
+	public String getEmailVerifycationToken() {
+		return emailVerifycationToken;
+	}
+	public void setEmailVerifycationToken(String emailVerifycationToken) {
+		this.emailVerifycationToken = emailVerifycationToken;
+	}
+	public Boolean getEmailVerificationStatus() {
+		return emailVerificationStatus;
+	}
+	public void setEmailVerificationStatus(Boolean emailVerificationStatus) {
+		this.emailVerificationStatus = emailVerificationStatus;
+	}
+	
+}
