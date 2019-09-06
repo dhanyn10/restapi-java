9dd8528041740b7751a9e7d296cb3932107ffa72
dhanyn10 <dhanyn.dhan@gmail.com>
2019-09-06 17:03:34 +0700

    user sign in: constants variable

---
+++ user-sign-in-7/src/main/java/com/test/users/service/UserService.java
@@ -0,0 +1,10 @@
+package com.test.users.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import com.test.users.shared.dto.UserDto;
+
+public interface UserService extends UserDetailsService{
+	
+	UserDto createUser(UserDto user);
+}
