478ed062289b6f92473b59a892207b128bed74b2
dhanyn10 <dhanyn.dhan@gmail.com>
2019-09-10 14:54:11 +0700

    stateless api 11

---
+++ restapi-stateless-11/src/main/java/com/test/users/security/AuthorizationFilter.java
@@ -0,0 +1,65 @@
+package com.test.users.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import io.jsonwebtoken.Jwts;
+
+public class AuthorizationFilter extends BasicAuthenticationFilter{
+	
+	public AuthorizationFilter(AuthenticationManager authmanager)
+	{
+		super(authmanager);
+	}
+	
+	@Override
+	protected void doFilterInternal(
+										HttpServletRequest req,
+										HttpServletResponse res,
+										FilterChain chain
+									) throws IOException, ServletException
+	{
+		String header = req.getHeader(SecurityConstants.headerString);
+		if(header == null || !header.startsWith(SecurityConstants.tokenPrefix))
+		{
+			chain.doFilter(req, res);
+			return;
+		}
+		
+		UsernamePasswordAuthenticationToken authentication = getAuthentication(req);
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		chain.doFilter(req, res);
+	}
+	
+	private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request)
+	{
+		String token = request.getHeader(SecurityConstants.headerString);
+		
+		if(token != null)
+		{
+			token = token.replace(SecurityConstants.tokenPrefix, "");
+			
+			String user = Jwts.parser()
+					.setSigningKey(SecurityConstants.tokenSecret)
+					.parseClaimsJws(token)
+					.getBody()
+					.getSubject();
+			if(user != null)
+			{
+				return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
+			}
+			return null;
+		}
+		return null;
+	}
+}
